a9c3de6a575ea6ffc55e11281678caa53f0f2f18
rudey794 <reubegan@gmail.com>
2026-09-08 15:35:40 +0100

    refactor: sort out spacing

--- 2025/7/solution.php
+++ 2025/7/solution.php
@@ -1,72 +1,72 @@
 <?php
-   function solve($file){
-      $lines = parse($file);
-      echo "part1: " . part1($lines) . "\n";
-      echo "part2: " . part2($lines) . "\n";
-   }
-   function part1($lines){
-      $total = 0;
-      echo $lines[0]."\n";
-      for($i = 1; $i < count($lines); $i++){
-            $beams = getBeamPositions($lines[$i - 1]);
-            $splits = splitBeams($lines, $i, $beams);
-            $total += $splits;
-            echo $lines[$i].": $splits (total: $total)\n";
-      }
-      return $total;
-   }
-   function part2($lines){
-      $memo = [];
-      $total = getPaths($memo, $lines, 1, strpos($lines[0], "S"));
-      return $total;
-   }
-   function getPaths(&$memo, $lines, $row, $column){
-      $paths = 0;
-      if(isset($memo[$row][$column])) return $memo[$row][$column];
-      $char = $lines[$row][$column] ?? "END";
-      if($char == "."){
-         $paths += getPaths($memo, $lines, $row + 1, $column);
-      } else if ($char == "^") {
-         $paths += getPaths($memo, $lines, $row, $column - 1);
-         $paths += getPaths($memo, $lines, $row, $column + 1);
-      } else {
-         $paths = 1;
-      }
-      
-      return $memo[$row][$column] = $paths;
-   }
-   function getBeamPositions($line){
-      $positions = [];
-      $length = strlen($line);
-      for($i=0; $i<$length; $i++){
-         if(in_array($line[$i], ["|", "S"])){
-            $positions[] = $i;
-         }
-      }
-      return $positions;
-   }
-   function splitBeams(&$lines, $i, $beams){
-      $splits = 0;
-      foreach($beams as $beam){
-         $char = $lines[$i][$beam];
-         if($char === "."){
-            $lines[$i][$beam] = "|";
-         } elseif($char === "^"){
-            $split = false;
-            foreach([-1, 1] as $mod){
-               if($lines[$i][$beam + $mod] == "."){
-                  $split = true;
-                  $lines[$i][$beam + $mod] = "|";
-               }
-            }
-            if($split) $splits++;
-         }
-      }
-      return $splits;
-   }
-   function parse($filename){
-      $content = file_get_contents($filename);
-      $lines = preg_split('/\R/', trim($content));
-      return $lines;
-   }
+	function solve($file){
+		$lines = parse($file);
+		echo "part1: " . part1($lines) . "\n";
+		echo "part2: " . part2($lines) . "\n";
+	}
+	function part1($lines){
+		$total = 0;
+		echo $lines[0]."\n";
+		for($i = 1; $i < count($lines); $i++){
+				$beams = getBeamPositions($lines[$i - 1]);
+				$splits = splitBeams($lines, $i, $beams);
+				$total += $splits;
+				echo $lines[$i].": $splits (total: $total)\n";
+		}
+		return $total;
+	}
+	function part2($lines){
+		$memo = [];
+		$total = getPaths($memo, $lines, 1, strpos($lines[0], "S"));
+		return $total;
+	}
+	function getPaths(&$memo, $lines, $row, $column){
+		$paths = 0;
+		if(isset($memo[$row][$column])) return $memo[$row][$column];
+		$char = $lines[$row][$column] ?? "END";
+		if($char == "."){
+			$paths += getPaths($memo, $lines, $row + 1, $column);
+		} else if ($char == "^") {
+			$paths += getPaths($memo, $lines, $row, $column - 1);
+			$paths += getPaths($memo, $lines, $row, $column + 1);
+		} else {
+			$paths = 1;
+		}
+		
+		return $memo[$row][$column] = $paths;
+	}
+	function getBeamPositions($line){
+		$positions = [];
+		$length = strlen($line);
+		for($i=0; $i<$length; $i++){
+			if(in_array($line[$i], ["|", "S"])){
+				$positions[] = $i;
+			}
+		}
+		return $positions;
+	}
+	function splitBeams(&$lines, $i, $beams){
+		$splits = 0;
+		foreach($beams as $beam){
+			$char = $lines[$i][$beam];
+			if($char === "."){
+				$lines[$i][$beam] = "|";
+			} elseif($char === "^"){
+				$split = false;
+				foreach([-1, 1] as $mod){
+				if($lines[$i][$beam + $mod] == "."){
+					$split = true;
+					$lines[$i][$beam + $mod] = "|";
+				}
+				}
+				if($split) $splits++;
+			}
+		}
+		return $splits;
+	}
+	function parse($filename){
+		$content = file_get_contents($filename);
+		$lines = preg_split('/\R/', trim($content));
+		return $lines;
+	}
 ?>
